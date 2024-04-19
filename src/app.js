e83151c7adc4ccfc34c19bc7c56e20fb9a5e72af
Gustavo Martins do Santos <gustavo@tfcsolutions.tech>
2024-04-18 21:46:52 -0300

    feat: add docker support to be able to test other docker services with this mock, fix lint errors and adds better logs to the application

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ const runServer = require('./server');
 
 async function main() {
   const app = await runServer();
-  const port = process.env.PORT || 3000
+  const port = process.env.PORT || 3000;
   app.listen(port, function () {
-    console.log(`CORS-enabled web server listening on port ${port}`)
-  })
+    console.log(`CORS-enabled web server listening on port ${port}`);
+  });
 }
 
 main();
--- src/server.js
+++ src/server.js
@@ -1,28 +1,49 @@
-
-require('dotenv').config()
+require('dotenv').config();
 
 const Express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 
 function startExpressApp() {
   const app = Express();
   app.use(Express.json({ limit: '1mb' }));
-  app.use(cors())
+  app.use(cors());
 
-  return app
+  return app;
+}
+
+function logRoutes(app) {
+  const routes = [];
+  app._router.stack.forEach((middleware) => {
+    if (middleware.route) {
+      const methods = Object.keys(middleware.route.methods)
+        .map((method) => method.toUpperCase())
+        .join(', ');
+      routes.push(`${methods} ${middleware.route.path}`);
+    } else if (middleware.name === 'router') {
+      middleware.handle.stack.forEach((handler) => {
+        const methods = Object.keys(handler.route.methods)
+          .map((method) => method.toUpperCase())
+          .join(', ');
+        routes.push(`${methods} ${handler.route.path}`);
+      });
+    }
+  });
+  console.log('\nRegistered Routes:', routes, '\n');
 }
 
 function runServer() {
   try {
-    const app = startExpressApp()
-    
-    app.get('/healthcheck', function (req, res, next) {
-      return res.status(200).send({status: 'ok'})
-    })
+    const app = startExpressApp();
 
-    return app
+    app.get('/healthcheck', function (req, res, next) {
+      console.log(`I'm alive!!`);
+      return res.status(200).send({ status: 'ok' });
+    });
+
+    logRoutes(app);
+    return app;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
